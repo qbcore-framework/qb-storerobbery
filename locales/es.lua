6e5cc2c79d07ef270919729e9dfef46dd7f2ae2c
Jean Carlos NUnez <3299025+jeann2013@users.noreply.github.com>
2022-09-02 21:27:32 -0500

    add two language english and spanish

---
+++ locales/es.lua
@@ -0,0 +1,26 @@
+local Translations = {
+    error = {
+        minimum_store_robbery_police = "No hay suficientes policia (%{MinimumStoreRobberyPolice} son requeridos)",
+        not_driver = "No eres un conductor",
+        demolish_vehicle = "No está permitido derribar vehículos ahora",
+        process_canceled = "Proceso cancelado..",
+        you_broke_the_lock_pick = "Has roto la ganzúa",
+        safe_code = "Código de seguridad: "
+    },
+    text = {
+        the_cash_register_is_empty = "La caja registradora está vacía",
+        try_combination = "~g~E~w~ - Ingresa la combinación",
+        safe_opened = "Caja abierta",
+        emptying_the_register= "Vaciando caja registradora.."
+    },
+    email = {
+        shop_robbery = "10-31 | Robo en progreso en Tienda",
+        someone_is_trying_to_rob_a_store = "Alguien está tratando de robar una tienda en %{street} (CAMARA ID: %{cameraId1})",
+        storerobbery_progress = "Robo en tienda en progreso"
+    },
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
